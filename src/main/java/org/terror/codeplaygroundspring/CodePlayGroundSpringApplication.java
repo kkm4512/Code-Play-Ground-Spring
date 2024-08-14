56e59aa1308b80e870f5290f4a43d1ecd73f454a
kkm4512 <nayoun440@naver.com>
2024-08-14 10:44:14 +0900

    init Data

---
+++ src/main/java/org/terror/codeplaygroundspring/CodePlayGroundSpringApplication.java
@@ -0,0 +1,13 @@
+package org.terror.codeplaygroundspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodePlayGroundSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodePlayGroundSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/terror/codeplaygroundspring/CodePlayGroundSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.terror.codeplaygroundspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodePlayGroundSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
