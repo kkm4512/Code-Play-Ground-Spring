2ea5d68e2e29825c14907b88d9d085c4206a02d1
kkm4512 <nayoun440@naver.com>
2024-08-31 17:57:44 +0900

    Global Exception Test

---
+++ src/main/java/org/terror/codeplaygroundspring/test5/MemberService.java
@@ -0,0 +1,18 @@
+package org.terror.codeplaygroundspring.test5;
+
+public class MemberService {
+    /**
+     * 유저의 이름, 비밀번호를 받아서 회원가입 하는 메서드
+     *
+     * @param username 유저의 이름
+     * @param password 유저의 비밀번호
+     * @return String 유저의 이름과 비밀번호를 더한 문자 반환
+     * @throws IllegalArgumentException 유저 이름 중복시, 발생 예외 처리
+     *
+     * @author 김경민
+     */
+    public String signup(String username, String password){
+        return "username" + "password";
+    }
+}
+
